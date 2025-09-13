25759e3b69a74deed8c553fb72e2cf8ebb0a86e6
Lijo Jacob <lijorobotics@gmail.com>
2025-09-13 07:18:34 -0400

    MyFirstOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstOpMode.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+ 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class MyFirstOpMode extends LinearOpMode {
+    private Gyroscope imu;
+    private DcMotor motorTest;
+    private DigitalChannel digitalTouch;
+    private DistanceSensor sensorColorRange;
+    private Servo servoTest;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Hello lijo here", 0);
+        telemetry.update();
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("frontleft");//rear right
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("backleft"); //front right
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("frontright");//slider
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("backright");//lift
+
+        // Reverse the right side motors
+        // Reverse left motors if you are using NeveRests
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        waitForStart();
+
+        telemetry.addData("Hello lijo here", 123);
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            telemetry.addData("Hello lijo here", 345);
+
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            telemetry.addData("frontLeftPower", frontLeftPower);
+            telemetry.addData("backLeftPower", backLeftPower);
+            telemetry.addData("frontRightPower", frontRightPower);
+            telemetry.addData("backRightPower", backRightPower);
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+            telemetry.update();
+        }
+    }
+}
